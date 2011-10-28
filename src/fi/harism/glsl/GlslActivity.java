3f41d861de1aa8f5427f8fa5197c7d1ed97bd018
harism <harism@gmail.com>
2011-10-28 14:21:15 +0300

    Initial commit.

---
+++ src/fi/harism/glsl/GlslActivity.java
@@ -0,0 +1,13 @@
+package fi.harism.glsl;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class GlslActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
