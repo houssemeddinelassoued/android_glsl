77fcdd6a04f113334b5774498e168aa973a23d5d
harism <harism@gmail.com>
2011-11-03 21:10:56 +0200

    Shader loading methods moved into GlslShader.

--- src/fi/harism/glsl/GlslUtils.java
+++ src/fi/harism/glsl/GlslUtils.java
@@ -1,54 +1,8 @@
 package fi.harism.glsl;
 
-import android.opengl.GLES20;
 import android.opengl.Matrix;
-import android.util.Log;
 
 public class GlslUtils {
-
-	public static final int createProgram(String vertexSource,
-			String fragmentSource) {
-		int vertexShader = loadShader(GLES20.GL_VERTEX_SHADER, vertexSource);
-		if (vertexShader == 0) {
-			return 0;
-		}
-
-		int pixelShader = loadShader(GLES20.GL_FRAGMENT_SHADER, fragmentSource);
-		if (pixelShader == 0) {
-			return 0;
-		}
-
-		int program = GLES20.glCreateProgram();
-		if (program != 0) {
-			GLES20.glAttachShader(program, vertexShader);
-			GLES20.glAttachShader(program, pixelShader);
-			GLES20.glLinkProgram(program);
-			int[] linkStatus = new int[1];
-			GLES20.glGetProgramiv(program, GLES20.GL_LINK_STATUS, linkStatus, 0);
-			if (linkStatus[0] != GLES20.GL_TRUE) {
-				Log.e("GlslUtils", GLES20.glGetProgramInfoLog(program));
-				GLES20.glDeleteProgram(program);
-				program = 0;
-			}
-		}
-		return program;
-	}
-
-	public static final int loadShader(int shaderType, String source) {
-		int shader = GLES20.glCreateShader(shaderType);
-		if (shader != 0) {
-			GLES20.glShaderSource(shader, source);
-			GLES20.glCompileShader(shader);
-			int[] compiled = new int[1];
-			GLES20.glGetShaderiv(shader, GLES20.GL_COMPILE_STATUS, compiled, 0);
-			if (compiled[0] == 0) {
-				Log.e("GlslUtils", GLES20.glGetShaderInfoLog(shader));
-				GLES20.glDeleteShader(shader);
-				shader = 0;
-			}
-		}
-		return shader;
-	}
 
 	public static final void setPerspectiveM(float[] m, float fovy,
 			float aspect, float zNear, float zFar) {
