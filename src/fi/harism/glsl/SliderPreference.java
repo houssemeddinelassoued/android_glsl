1a8155e8cb64e00f20d1c9bbe61694a4896f9f0d
harism <harism@gmail.com>
2011-11-03 18:55:05 +0200

    Settings/preferences updated.

--- src/fi/harism/glsl/SliderPreference.java
+++ src/fi/harism/glsl/SliderPreference.java
@@ -3,89 +3,89 @@ package fi.harism.glsl;
 import android.content.Context;
 import android.content.res.TypedArray;
 import android.preference.DialogPreference;
-import android.preference.PreferenceManager;
 import android.util.AttributeSet;
 import android.view.View;
 import android.widget.SeekBar;
 
 public class SliderPreference extends DialogPreference {
-	
-	private int mValue;
-	private int mMinValue;
-	private int mMaxValue;
-	private int mFloatDivider;
-	
+
+	private float mValue;
+	private float mValueMin;
+	private float mValueMax;
+	private float mValuePrecision;
+
 	private String mTitle;
-	
+
 	public SliderPreference(Context context, AttributeSet attrs, int defStyle) {
 		super(context, attrs, defStyle);
-		
-		TypedArray ta = context.obtainStyledAttributes(attrs, R.styleable.SliderPreference);
-		mMinValue = ta.getInt(R.styleable.SliderPreference_minValue, 0);
-		mMaxValue = ta.getInt(R.styleable.SliderPreference_maxValue, 100);
-		mFloatDivider = ta.getInt(R.styleable.SliderPreference_floatDivider, 0);
-		
+
+		TypedArray ta = context.obtainStyledAttributes(attrs,
+				R.styleable.SliderPreference);
+		mValueMin = ta.getFloat(R.styleable.SliderPreference_valueMin, 0);
+		mValueMax = ta.getFloat(R.styleable.SliderPreference_valueMax, 100);
+		mValuePrecision = ta.getFloat(
+				R.styleable.SliderPreference_valuePrecision, 1);
+
 		mTitle = getTitle().toString();
 		setDialogLayoutResource(R.layout.slider);
 	}
-	
+
 	public SliderPreference(Context context, AttributeSet attrs) {
 		this(context, attrs, 0);
 	}
-	
+
 	@Override
-	public void onAttachedToHierarchy(PreferenceManager preferenceManager) {
-		super.onAttachedToHierarchy(preferenceManager);
-		if (mFloatDivider == 0) {
-			mValue = getPersistedInt(0);
+	protected Object onGetDefaultValue(TypedArray attrs, int index) {
+		return attrs.getFloat(index, 0);
+	}
+
+	@Override
+	protected void onSetInitialValue(boolean restorePersistedValue,
+			Object defaultValue) {
+		if (restorePersistedValue) {
+			mValue = getPersistedFloat(0);
 		} else {
-			mValue = (int)(getPersistedFloat(0) * mFloatDivider);
+			mValue = (Float) defaultValue;
 		}
 		updateTitle();
 	}
-	
+
 	@Override
 	protected void onBindDialogView(View v) {
 		super.onBindDialogView(v);
-		SeekBar seekBar = (SeekBar)v.findViewById(R.id.slider);
-		seekBar.setMax(mMaxValue - mMinValue);
-		seekBar.setProgress(mValue - mMinValue);
+		SeekBar seekBar = (SeekBar) v.findViewById(R.id.slider);
+		seekBar.setMax((int) ((mValueMax - mValueMin) / mValuePrecision));
+		seekBar.setProgress((int) ((mValue - mValueMin) / mValuePrecision));
 		seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
 			@Override
 			public void onStopTrackingTouch(SeekBar sb) {
 			}
+
 			@Override
 			public void onStartTrackingTouch(SeekBar sb) {
 			}
+
 			@Override
-			public void onProgressChanged(SeekBar sb, int newValue, boolean fromUser) {
-				mValue = newValue + mMinValue;
+			public void onProgressChanged(SeekBar sb, int newValue,
+					boolean fromUser) {
+				mValue = (newValue * mValuePrecision) + mValueMin;
 				updateTitle();
 			}
 		});
 	}
-	
+
 	@Override
 	protected void onDialogClosed(boolean positiveResult) {
-		if (mFloatDivider == 0) {
-			persistInt(mValue);
-		} else {
-			persistFloat((float)mValue / mFloatDivider);
-		}
+		persistFloat(mValue);
 	}
-	
+
 	private void updateTitle() {
-		String title;
-		if (mFloatDivider == 0) {
-			title = String.format(mTitle, mValue);
-		} else {
-			title = String.format(mTitle, (float)mValue / mFloatDivider);
-		}
+		String title = String.format(mTitle, mValue);
 		setTitle(title);
 		setDialogTitle(title);
 		if (getDialog() != null) {
 			getDialog().setTitle(title);
 		}
 	}
-	
+
 }
