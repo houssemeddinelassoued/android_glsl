13cec33672772f4e3c95a50312a9c36363bda11a
harism <harism@gmail.com>
2011-11-04 20:22:52 +0200

    Reset default preferences preference added.

--- src/fi/harism/glsl/GlslPreferenceActivity.java
+++ src/fi/harism/glsl/GlslPreferenceActivity.java
@@ -1,14 +1,36 @@
 package fi.harism.glsl;
 
 import android.os.Bundle;
+import android.preference.Preference;
 import android.preference.PreferenceActivity;
+import android.preference.PreferenceManager;
 
-public final class GlslPreferenceActivity extends PreferenceActivity {
+public final class GlslPreferenceActivity extends PreferenceActivity implements
+		Preference.OnPreferenceClickListener {
 
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		addPreferencesFromResource(R.xml.preferences);
+
+		String key = getString(R.string.key_reset);
+		Preference p = getPreferenceScreen().findPreference(key);
+		if (p != null) {
+			p.setOnPreferenceClickListener(this);
+		}
+	}
+
+	@Override
+	public boolean onPreferenceClick(Preference preference) {
+		String key = getString(R.string.key_reset);
+		if (preference.getKey().equals(key)) {
+			preference.getEditor().clear().commit();
+			PreferenceManager.setDefaultValues(GlslPreferenceActivity.this,
+					R.xml.preferences, true);
+			finish();
+			return true;
+		}
+		return false;
 	}
 
 }
