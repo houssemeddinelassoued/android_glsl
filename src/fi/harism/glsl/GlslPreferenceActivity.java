82a30d376a603ece52ca2e86cd3ec19573579f0e
harism <harism@gmail.com>
2011-11-04 14:06:06 +0200

    Declaring changed to final class.

--- src/fi/harism/glsl/GlslPreferenceActivity.java
+++ src/fi/harism/glsl/GlslPreferenceActivity.java
@@ -3,7 +3,7 @@ package fi.harism.glsl;
 import android.os.Bundle;
 import android.preference.PreferenceActivity;
 
-public class GlslPreferenceActivity extends PreferenceActivity {
+public final class GlslPreferenceActivity extends PreferenceActivity {
 
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
