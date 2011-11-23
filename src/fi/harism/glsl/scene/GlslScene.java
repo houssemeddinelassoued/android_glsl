2bbf8af5b9d43d112f1f8d3c452ccde8a54bede9
harism <harism@gmail.com>
2011-11-23 18:15:47 +0200

    Rand() method taken into use.

--- src/fi/harism/glsl/scene/GlslScene.java
+++ src/fi/harism/glsl/scene/GlslScene.java
@@ -20,36 +20,71 @@ import java.util.Vector;
 
 import fi.harism.glsl.GlslCamera;
 
+/**
+ * Class for encapsulating scene related objects, lights etc.
+ */
 public final class GlslScene {
 
 	private GlslAnimator mAnimator = new GlslAnimator();
 	private Vector<GlslObject> mObjects = new Vector<GlslObject>();
 	private Vector<GlslLight> mLights = new Vector<GlslLight>();
 
+	/**
+	 * Updates objects based on their animation values.
+	 * 
+	 * @param time
+	 *            current time in millis
+	 */
 	public void animate(long time) {
 		mAnimator.animate(time);
 	}
 
+	/**
+	 * Renders objects into the scene
+	 * 
+	 * @param mData
+	 *            Shader id values to use
+	 */
 	public void draw(GlslShaderIds mData) {
 		for (GlslObject object : mObjects) {
 			object.draw(mData);
 		}
 	}
 
+	/**
+	 * Returns light with given index. Index is a value between [0,
+	 * getLightCount() - 1].
+	 * 
+	 * @param idx
+	 *            Light index
+	 * @return Light with given index
+	 */
 	public GlslLight getLight(int idx) {
 		return mLights.get(idx);
 	}
 
+	/**
+	 * Returns current light count
+	 * 
+	 * @return number of lights
+	 */
 	public int getLightCount() {
 		return mLights.size();
 	}
 
-	public void initSceneBoxes1(int lightCount) {
+	/**
+	 * Helper method for creating example scene.
+	 * 
+	 * @param camera
+	 *            Camera for adding animation to it.
+	 * @param lightCount
+	 *            Number of lights to add to the scene
+	 */
+	public void initSceneBoxes1(GlslCamera camera, int lightCount) {
 		reset();
 
 		GlslObject rootObject = new GlslObject();
 		mObjects.add(rootObject);
-		mAnimator.setRotation(rootObject, 3000, 5000, 6000);
 
 		GlslCube cube = new GlslCube();
 		cube.setScaling(15f);
@@ -99,7 +134,8 @@ public final class GlslScene {
 		cube.setColor((float) Math.random(), (float) Math.random(),
 				(float) Math.random());
 		rootObject.addChild(cube);
-		mAnimator.setRotation(cube, 4000, -5000, 7000);
+		mAnimator.setRotation(cube, mAnimator.new RotationData(10000, -15000,
+				17000));
 
 		for (int i = 0; i < lightCount; ++i) {
 			GlslLight light = new GlslLight();
@@ -110,13 +146,21 @@ public final class GlslScene {
 		}
 	}
 
+	/**
+	 * Helper method for creating example scene.
+	 * 
+	 * @param camera
+	 *            Camera for adding animation to it.
+	 * @param lightCount
+	 *            Number of lights to add to the scene
+	 */
 	public void initSceneBoxes2(GlslCamera camera, int lightCount) {
 		reset();
 
-		final int CUBE_SCROLLER_COUNT = 50;
+		final int CUBE_SCROLLER_COUNT = 20;
 		final int CUBE_ARCH_COUNT = 10;
-		final float CUBE_SCROLLER_NEAR = 30f;
-		final float CUBE_SCROLLER_FAR = -30f;
+		final float CUBE_SCROLLER_NEAR = 10f;
+		final float CUBE_SCROLLER_FAR = -10f;
 
 		GlslObject rootObject = new GlslObject();
 		mObjects.add(rootObject);
@@ -131,16 +175,11 @@ public final class GlslScene {
 		for (int idx = 0; idx < CUBE_SCROLLER_COUNT; ++idx) {
 			GlslCube cube = new GlslCube();
 
-			cube.setScaling((float) (.4f * Math.random() + .8f));
-			cube.setRotation((float) (360 * Math.random()),
-					(float) (360 * Math.random()),
-					(float) (360 * Math.random()));
-			cube.setPosition((float) (2 * Math.random() - 1),
-					(float) (2 * Math.random() - 1), (float) Math.random()
-							* (CUBE_SCROLLER_NEAR - CUBE_SCROLLER_FAR)
-							- CUBE_SCROLLER_NEAR);
-			cube.setColor((float) Math.random(), (float) Math.random(),
-					(float) Math.random());
+			cube.setScaling(rand(.8f, 1.2f));
+			cube.setRotation(rand(0f, 360f), rand(0f, 360f), rand(0f, 360f));
+			cube.setPosition(rand(-1f, 1f), rand(-1f, 1f),
+					rand(CUBE_SCROLLER_NEAR, CUBE_SCROLLER_FAR));
+			cube.setColor(rand(.2f, 1f), rand(.2f, 1f), rand(.2f, 1f));
 			rootObject.addChild(cube);
 		}
 
@@ -149,14 +188,11 @@ public final class GlslScene {
 
 			double t = Math.PI * idx / (CUBE_ARCH_COUNT - 1);
 
-			cube.setScaling((float) (1f * Math.random() + 1f));
-			cube.setRotation((float) (360 * Math.random()),
-					(float) (360 * Math.random()),
-					(float) (360 * Math.random()));
+			cube.setScaling(rand(1f, 2f));
+			cube.setRotation(rand(0f, 360f), rand(0f, 360f), rand(0f, 360f));
 			cube.setPosition((float) (5 * Math.cos(t)),
 					(float) (5 * Math.sin(t)), 0f);
-			cube.setColor((float) Math.random(), (float) Math.random(),
-					(float) Math.random());
+			cube.setColor(rand(.2f, 1f), rand(.2f, 1f), rand(.2f, 1f));
 			rootObject.addChild(cube);
 		}
 
@@ -177,12 +213,24 @@ public final class GlslScene {
 		mAnimator.setPath(camera, path);
 	}
 
+	/**
+	 * Clears all objects from this scene.
+	 */
 	public void reset() {
 		mAnimator.clear();
 		mObjects.clear();
 		mLights.clear();
 	}
 
+	/**
+	 * Updates object hierarchy matrices with given view and projection
+	 * matrices.
+	 * 
+	 * @param viewM
+	 *            View matrix
+	 * @param projM
+	 *            Projection matrix
+	 */
 	public void setMVP(float[] viewM, float[] projM) {
 		for (GlslObject object : mObjects) {
 			object.setMVP(viewM, projM);
@@ -190,6 +238,19 @@ public final class GlslScene {
 		for (GlslLight light : mLights) {
 			light.setMVP(viewM);
 		}
+	}
+
+	/**
+	 * Private helper method for calculating random values.
+	 * 
+	 * @param min
+	 *            Minimum value
+	 * @param max
+	 *            Maximum value
+	 * @return Value between [min, max)
+	 */
+	private float rand(float min, float max) {
+		return (float) (min + Math.random() * (max - min));
 	}
 
 }
