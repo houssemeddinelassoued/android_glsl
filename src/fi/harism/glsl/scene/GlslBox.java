99f582f38e2ec25518e0083dd0b7f64ef78df97b
harism <harism@gmail.com>
2011-12-05 01:15:56 +0200

    Modified to handle shadow stencil rendering.

--- src/fi/harism/glsl/scene/GlslBox.java
+++ src/fi/harism/glsl/scene/GlslBox.java
@@ -24,20 +24,69 @@ import android.opengl.GLES20;
 
 public final class GlslBox extends GlslObject {
 
+	private static final int FACE_COUNT = 6;
+	private static final int VERTICES_PER_FACE = 8;
+	private static final int FLOATS_PER_VERTEX = 3;
 	private static final int FLOAT_SIZE_BYTES = 4;
-	private static final int DATA_STRIDE_FLOATS = 9;
-	private static final int DATA_STRIDE_BYTES = DATA_STRIDE_FLOATS
-			* FLOAT_SIZE_BYTES;
-	private static final int DATA_POS_OFFSET = 0;
-	private static final int DATA_COL_OFFSET = 3;
-	private static final int DATA_NORMAL_OFFSET = 6;
 
 	private FloatBuffer mVertexBuffer;
+	private FloatBuffer mNormalBuffer;
+	private FloatBuffer mColorBuffer;
+	private ByteBuffer mIndexBuffer;
+	private ByteBuffer mShadowIndexBuffer;
 
 	public GlslBox() {
-		ByteBuffer buffer = ByteBuffer
-				.allocateDirect(6 * 6 * DATA_STRIDE_BYTES);
+		int sz = FACE_COUNT * VERTICES_PER_FACE * FLOATS_PER_VERTEX
+				* FLOAT_SIZE_BYTES;
+		ByteBuffer buffer = ByteBuffer.allocateDirect(sz);
 		mVertexBuffer = buffer.order(ByteOrder.nativeOrder()).asFloatBuffer();
+		buffer = ByteBuffer.allocateDirect(sz);
+		mNormalBuffer = buffer.order(ByteOrder.nativeOrder()).asFloatBuffer();
+		buffer = ByteBuffer.allocateDirect(sz);
+		mColorBuffer = buffer.order(ByteOrder.nativeOrder()).asFloatBuffer();
+
+		mIndexBuffer = ByteBuffer.allocateDirect(6 * 6);
+		mIndexBuffer.position(0);
+		for (int i = 0; i < 6; ++i) {
+			mIndexBuffer.put((byte) (i * 8 + 0));
+			mIndexBuffer.put((byte) (i * 8 + 2));
+			mIndexBuffer.put((byte) (i * 8 + 4));
+			mIndexBuffer.put((byte) (i * 8 + 2));
+			mIndexBuffer.put((byte) (i * 8 + 6));
+			mIndexBuffer.put((byte) (i * 8 + 4));
+		}
+
+		mShadowIndexBuffer = ByteBuffer.allocateDirect(6 * 4 * 6);
+		mShadowIndexBuffer.position(0);
+		for (int i = 0; i < 6; ++i) {
+			mShadowIndexBuffer.put((byte) (i * 8 + 0));
+			mShadowIndexBuffer.put((byte) (i * 8 + 1));
+			mShadowIndexBuffer.put((byte) (i * 8 + 2));
+			mShadowIndexBuffer.put((byte) (i * 8 + 1));
+			mShadowIndexBuffer.put((byte) (i * 8 + 3));
+			mShadowIndexBuffer.put((byte) (i * 8 + 2));
+
+			mShadowIndexBuffer.put((byte) (i * 8 + 2));
+			mShadowIndexBuffer.put((byte) (i * 8 + 3));
+			mShadowIndexBuffer.put((byte) (i * 8 + 6));
+			mShadowIndexBuffer.put((byte) (i * 8 + 3));
+			mShadowIndexBuffer.put((byte) (i * 8 + 7));
+			mShadowIndexBuffer.put((byte) (i * 8 + 6));
+
+			mShadowIndexBuffer.put((byte) (i * 8 + 6));
+			mShadowIndexBuffer.put((byte) (i * 8 + 7));
+			mShadowIndexBuffer.put((byte) (i * 8 + 4));
+			mShadowIndexBuffer.put((byte) (i * 8 + 7));
+			mShadowIndexBuffer.put((byte) (i * 8 + 5));
+			mShadowIndexBuffer.put((byte) (i * 8 + 4));
+
+			mShadowIndexBuffer.put((byte) (i * 8 + 4));
+			mShadowIndexBuffer.put((byte) (i * 8 + 5));
+			mShadowIndexBuffer.put((byte) (i * 8 + 0));
+			mShadowIndexBuffer.put((byte) (i * 8 + 5));
+			mShadowIndexBuffer.put((byte) (i * 8 + 1));
+			mShadowIndexBuffer.put((byte) (i * 8 + 0));
+		}
 
 		setNormal(0, 0f, 1f, 0f);
 		setNormal(1, 0f, -1f, 0f);
@@ -52,22 +101,22 @@ public final class GlslBox extends GlslObject {
 
 	@Override
 	public void render(GlslShaderIds ids) {
-
 		super.render(ids);
 
-		mVertexBuffer.position(DATA_POS_OFFSET);
+		int stride = FLOATS_PER_VERTEX * FLOAT_SIZE_BYTES;
+		mVertexBuffer.position(0);
 		GLES20.glVertexAttribPointer(ids.aPosition, 3, GLES20.GL_FLOAT, false,
-				DATA_STRIDE_BYTES, mVertexBuffer);
+				stride, mVertexBuffer);
 		GLES20.glEnableVertexAttribArray(ids.aPosition);
 
-		mVertexBuffer.position(DATA_NORMAL_OFFSET);
+		mNormalBuffer.position(0);
 		GLES20.glVertexAttribPointer(ids.aNormal, 3, GLES20.GL_FLOAT, false,
-				DATA_STRIDE_BYTES, mVertexBuffer);
+				stride, mNormalBuffer);
 		GLES20.glEnableVertexAttribArray(ids.aNormal);
 
-		mVertexBuffer.position(DATA_COL_OFFSET);
+		mColorBuffer.position(0);
 		GLES20.glVertexAttribPointer(ids.aColor, 3, GLES20.GL_FLOAT, false,
-				DATA_STRIDE_BYTES, mVertexBuffer);
+				stride, mColorBuffer);
 		GLES20.glEnableVertexAttribArray(ids.aColor);
 
 		GLES20.glUniformMatrix4fv(ids.uMVMatrix, 1, false, getModelViewM(), 0);
@@ -75,21 +124,50 @@ public final class GlslBox extends GlslObject {
 				getModelViewProjM(), 0);
 		GLES20.glUniformMatrix4fv(ids.uNormalMatrix, 1, false, getNormalM(), 0);
 
-		GLES20.glDrawArrays(GLES20.GL_TRIANGLES, 0, 6 * 6);
+		mIndexBuffer.position(0);
+		GLES20.glDrawElements(GLES20.GL_TRIANGLES, 6 * 6,
+				GLES20.GL_UNSIGNED_BYTE, mIndexBuffer);
+		// GLES20.glDrawArrays(GLES20.GL_TRIANGLES, 0, 6 * 6);
+	}
+
+	@Override
+	public void renderShadow(int uMVMatrix, int uMVPMatrix, int uNormalMatrix,
+			int aPosition, int aNormal) {
+		super.renderShadow(uMVMatrix, uMVPMatrix, uNormalMatrix, aPosition,
+				aNormal);
+
+		int stride = FLOATS_PER_VERTEX * FLOAT_SIZE_BYTES;
+		mVertexBuffer.position(0);
+		GLES20.glVertexAttribPointer(aPosition, 3, GLES20.GL_FLOAT, false,
+				stride, mVertexBuffer);
+		GLES20.glEnableVertexAttribArray(aPosition);
+
+		mNormalBuffer.position(0);
+		GLES20.glVertexAttribPointer(aNormal, 3, GLES20.GL_FLOAT, false,
+				stride, mNormalBuffer);
+		GLES20.glEnableVertexAttribArray(aNormal);
+
+		GLES20.glUniformMatrix4fv(uMVMatrix, 1, false, getModelViewM(), 0);
+		GLES20.glUniformMatrix4fv(uMVPMatrix, 1, false, getModelViewProjM(), 0);
+		GLES20.glUniformMatrix4fv(uNormalMatrix, 1, false, getNormalM(), 0);
+
+		mShadowIndexBuffer.position(0);
+		GLES20.glDrawElements(GLES20.GL_TRIANGLES, 6 * 4 * 6,
+				GLES20.GL_UNSIGNED_BYTE, mShadowIndexBuffer);
 	}
 
 	public void setColor(float r, float g, float b) {
-		for (int i = 0; i < 6; ++i) {
+		for (int i = 0; i < FACE_COUNT; ++i) {
 			setColor(i, r, g, b);
 		}
 	}
 
-	public void setColor(int side, float r, float g, float b) {
-		int i = DATA_COL_OFFSET + side * DATA_STRIDE_FLOATS * 6;
-		for (int j = 0; j < 6; ++j, i += DATA_STRIDE_FLOATS) {
-			mVertexBuffer.put(i, r);
-			mVertexBuffer.put(i + 1, g);
-			mVertexBuffer.put(i + 2, b);
+	public void setColor(int face, float r, float g, float b) {
+		int i = face * VERTICES_PER_FACE * FLOATS_PER_VERTEX;
+		for (int j = 0; j < VERTICES_PER_FACE; ++j) {
+			mColorBuffer.put(i + (j * FLOATS_PER_VERTEX), r);
+			mColorBuffer.put(i + (j * FLOATS_PER_VERTEX) + 1, g);
+			mColorBuffer.put(i + (j * FLOATS_PER_VERTEX) + 2, b);
 		}
 	}
 
@@ -109,47 +187,52 @@ public final class GlslBox extends GlslObject {
 		setSideCoordinates(5, 0, 1, 2, x1, y1, x2, y2, z2);
 	}
 
-	private void setNormal(int side, float x, float y, float z) {
-		int i = DATA_NORMAL_OFFSET + side * DATA_STRIDE_FLOATS * 6;
-		for (int j = 0; j < 6; ++j, i += DATA_STRIDE_FLOATS) {
-			mVertexBuffer.put(i, x);
-			mVertexBuffer.put(i + 1, y);
-			mVertexBuffer.put(i + 2, z);
+	private void setNormal(int face, float x, float y, float z) {
+		int i = face * VERTICES_PER_FACE * FLOATS_PER_VERTEX;
+		for (int j = 0; j < VERTICES_PER_FACE; j += 2) {
+			mNormalBuffer.put(i + (j * FLOATS_PER_VERTEX), x);
+			mNormalBuffer.put(i + (j * FLOATS_PER_VERTEX) + 1, y);
+			mNormalBuffer.put(i + (j * FLOATS_PER_VERTEX) + 2, z);
+			mNormalBuffer.put(i + (j * FLOATS_PER_VERTEX) + 3, 0);
+			mNormalBuffer.put(i + (j * FLOATS_PER_VERTEX) + 4, 0);
+			mNormalBuffer.put(i + (j * FLOATS_PER_VERTEX) + 5, 0);
 		}
 	}
 
-	private void setSideCoordinates(int side, int is, int it, int iu, float s1,
+	private void setSideCoordinates(int face, int is, int it, int iu, float s1,
 			float t1, float s2, float t2, float u) {
-		int i = DATA_POS_OFFSET + side * DATA_STRIDE_FLOATS * 6;
+		int i = face * VERTICES_PER_FACE * FLOATS_PER_VERTEX;
 
 		mVertexBuffer.put(i + is, s1);
 		mVertexBuffer.put(i + it, t1);
 		mVertexBuffer.put(i + iu, u);
-		i += DATA_STRIDE_FLOATS;
+		mVertexBuffer.put(i + is + 3, s1);
+		mVertexBuffer.put(i + it + 3, t1);
+		mVertexBuffer.put(i + iu + 3, u);
+		i += 2 * FLOATS_PER_VERTEX;
 
 		mVertexBuffer.put(i + is, s1);
 		mVertexBuffer.put(i + it, t2);
 		mVertexBuffer.put(i + iu, u);
-		i += DATA_STRIDE_FLOATS;
+		mVertexBuffer.put(i + is + 3, s1);
+		mVertexBuffer.put(i + it + 3, t2);
+		mVertexBuffer.put(i + iu + 3, u);
+		i += 2 * FLOATS_PER_VERTEX;
 
 		mVertexBuffer.put(i + is, s2);
 		mVertexBuffer.put(i + it, t1);
 		mVertexBuffer.put(i + iu, u);
-		i += DATA_STRIDE_FLOATS;
-
-		mVertexBuffer.put(i + is, s1);
-		mVertexBuffer.put(i + it, t2);
-		mVertexBuffer.put(i + iu, u);
-		i += DATA_STRIDE_FLOATS;
+		mVertexBuffer.put(i + is + 3, s2);
+		mVertexBuffer.put(i + it + 3, t1);
+		mVertexBuffer.put(i + iu + 3, u);
+		i += 2 * FLOATS_PER_VERTEX;
 
 		mVertexBuffer.put(i + is, s2);
 		mVertexBuffer.put(i + it, t2);
 		mVertexBuffer.put(i + iu, u);
-		i += DATA_STRIDE_FLOATS;
-
-		mVertexBuffer.put(i + is, s2);
-		mVertexBuffer.put(i + it, t1);
-		mVertexBuffer.put(i + iu, u);
+		mVertexBuffer.put(i + is + 3, s2);
+		mVertexBuffer.put(i + it + 3, t2);
+		mVertexBuffer.put(i + iu + 3, u);
 	}
 
 }
